257ee93ccf96f2e15dd7977772ee7cd39ae51498
Earid A <earid777@gmail.com>
2023-12-21 22:31:18 +0600

    Update steps.js

--- a1-todo/steps.js
+++ a1-todo/steps.js
@@ -1,13 +1,13 @@
-// add state in input
+// todo input > add state in the input
 const [inputText, setInputText] = useState('');
 <input onChange={ e => {(setInputText(e.target.value))}} />
-// send data child to parent 
+// app >  send data child to parent 
    const [listToDo, setListToDo] = useState([]);
     let addList = (inputText) => {
         setListToDo([...listToDo, inputText])
     }
-// add props in child component
-// spread oparetor to add in data in array
+//Add props in the child component
+// spread operator to add data in an array
 return (
    <div> 
       <TodoInput addList={addList} />
