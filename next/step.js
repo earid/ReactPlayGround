e72a4a77e2c8c4aee03fcfe0bfd16b46348da555
Earid A <earid777@gmail.com>
2024-01-01 21:23:40 +0600

    Update step.js

--- next/step.js
+++ next/step.js
@@ -1,1 +1,21 @@
 npx create-next-app@latest
+  
+1) menu initial
+const Links = () => {
+  const links = [
+    { title: "Home", path: "/" },
+    { title: "About Us", path: "/about" },
+    { title: "Contact", path: "/contact" },
+    { title: "Blog", path: "/blog" },
+  ];
+
+  return (
+    <div className={styles.links}>
+      {links.map((link) => (
+        <Link href={link.path} key={link.title}>
+          {link.title}
+        </Link>
+      ))}
+    </div>
+  );
+};
